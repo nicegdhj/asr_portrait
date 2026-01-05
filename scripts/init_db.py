18db43e5fb2a411ec1490f458d98f832a46a9718
HeJia <nicehejia@gmail.com>
2026-01-05 18:39:29 +0800

    feat: 添加部署后访问检测脚本 check_access.sh
    - 自动检测内网/公网 IP 地址
    - 检测 Docker 容器运行状态
    - 检测端口监听状态 (80/8000/5432)
    - 检测服务健康状态
    - 检测防火墙配置及端口开放情况
    - 生成局域网和公网访问地址

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -2,6 +2,15 @@
 Author: HeJia nicehejia@gmail.com
 Date: 2025-12-04 14:48:18
 LastEditors: HeJia nicehejia@gmail.com
+LastEditTime: 2026-01-05 18:34:10
+FilePath: /potrait/scripts/init_db.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+"""
+
+"""
+Author: HeJia nicehejia@gmail.com
+Date: 2025-12-04 14:48:18
+LastEditors: HeJia nicehejia@gmail.com
 LastEditTime: 2025-12-23 14:55:30
 FilePath: /potrait/scripts/init_db.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
